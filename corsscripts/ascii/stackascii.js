e57e6f207ffcabd2201f8d6c696310f09eda5c63
Edmund Farrow <edmund.farrow@ed.ac.uk>
2026-05-18 17:02:42 +0100

    iss1748 - Remove backticks from around answer. Add scroll fro overflow.

--- corsscripts/ascii/stackascii.js
+++ corsscripts/ascii/stackascii.js
@@ -1,55 +1,57 @@
 export default function init(inputIds) {
-	const markdownContainerId = inputIds[0];
-	const answerContainerId = inputIds[1];
+    const markdownContainerId = inputIds[0];
+    const answerContainerId = inputIds[1];
 
-	function renderMath() {
-		const raw = document.getElementById(markdownContainerId).value.trim();
-		const output = document.getElementById('asciiContainerRow');
-		if (!raw) {
-			output.innerHTML = '';
-			return;
-		}
+    function renderMath() {
+        const raw = document.getElementById(markdownContainerId).value.trim();
+        const output = document.getElementById('asciiContainerRow');
+        if (!raw) {
+            output.innerHTML = '';
+            return;
+        }
 
-		convertMarkdown(raw);
+        convertMarkdown(raw);
 
-		// Tell MathJax to typeset only this element
-		if (typeof MathJax.typesetPromise === 'function') {
-			MathJax.typesetPromise([output]); //MathJax 3
-		} else {
-			MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'asciiContainerRow']);  //MathJax 2
-		}
+        // Tell MathJax to typeset only this element
+        if (typeof MathJax.typesetPromise === 'function') {
+            MathJax.typesetPromise([output]); //MathJax 3
+        } else {
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, 'asciiContainerRow']);  //MathJax 2
+        }
 
-		let lines = raw.split('\n');
-		lines.reverse();
-		for (const line of lines) {
-			const trimmed = line.trim()
-			if (['```', '/)', ''].includes(trimmed)) {
-				continue;
-			}
-			if (/^f\s*\(\s*x\s*\)\s*=\s*/.test(trimmed)) {
-				const answerEl = document.getElementById(answerContainerId);
-				answerEl.value = trimmed.replace(/^f\s*\(\s*x\s*\)\s*=\s*/, '');
-				answerEl.dispatchEvent(new Event("change"));
-				break;
-			}
-		}
-	}
+        let lines = raw.split('\n');
+        lines.reverse();
+        for (const line of lines) {
+            const trimmed = line.trim()
+            if (['```', '/)', ''].includes(trimmed)) {
+                continue;
+            }
 
-	let debounceTimer;
-	document.getElementById(markdownContainerId).addEventListener('change', () => {
-		clearTimeout(debounceTimer);
-		debounceTimer = setTimeout(renderMath, 100); // debounce 100ms
-	});
-	renderMath();
+            // Convert 'f(x) = answer' to 'answer', removing backticks from beginning and end.
+            if (/^`*f\(x\)\s*=\s*/.test(trimmed)) {
+                const answerEl = document.getElementById(answerContainerId);
+                answerEl.value = trimmed.replace(/^`*f\(x\)\s*=\s*|`+$/g, '');
+                answerEl.dispatchEvent(new Event("change"));
+                break;
+            }
+        }
+    }
+
+    let debounceTimer;
+    document.getElementById(markdownContainerId).addEventListener('change', () => {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(renderMath, 100); // debounce 100ms
+    });
+    renderMath();
 }
 
 // mdItPluginTex.tex must come before markdownitrules.
 const previewMarkdownConverter = window.markdownit({ html: true })
-	.use(window.markdownitSub)
-	.use(window.mdItPluginTex.tex, { render: (content) => content, delimiters: 'brackets' })
-	.use(window.markdownitrules);
+    .use(window.markdownitSub)
+    .use(window.mdItPluginTex.tex, { render: (content) => content, delimiters: 'brackets' })
+    .use(window.markdownitrules);
 
 function convertMarkdown(markdown) {
-	const html = previewMarkdownConverter.render(markdown);
-	document.getElementById('asciiContainerRow').innerHTML = html;
+    const html = previewMarkdownConverter.render(markdown);
+    document.getElementById('asciiContainerRow').innerHTML = html;
 }
