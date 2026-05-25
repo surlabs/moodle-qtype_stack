17bc5ca70e9e2581bbeab766ecb85585bcde408f
sangwinc <C.J.Sangwin@ed.ac.uk>
2026-05-25 16:29:36 +0100

    Fix bug in lateswrap with displaystyle.

--- corsscripts/ascii/filters/010_latexwrap.js
+++ corsscripts/ascii/filters/010_latexwrap.js
@@ -10,10 +10,11 @@ import findtextindex from './findtextindex.js';
 export default function latexwrap(lines) {
     const output = [`\\[\\begin{align*}`];
     for (let str of lines) {
+        str = str.trim();
         // 3. Find first occurrence of \text{ and bump that to the next column.
         const matchtxt = findtextindex(str, ['\\text{'], [`\\text{or}`, `\\text{and}`, `\\text{if}`]);
         if (matchtxt) {
-            str = str.slice(0, matchtxt) + '& &' + str.slice(matchtxt);
+            str = str.slice(0, matchtxt) + "& &" + str.slice(matchtxt);
         }
         // 2. Find first occurrence of equals, inequality etc. and bump rest to the next column.
         const bracest = [
@@ -26,18 +27,25 @@ export default function latexwrap(lines) {
         const matcheq = findtextindex(str, braces);
         // Zero vs false issue.
         if (matcheq !== false) {
-            str = str.slice(0, matcheq) + '&' + str.slice(matcheq);
+            str = str.slice(0, matcheq) + "&" + str.slice(matcheq);
         } else {
-            str = str + `&`;
+            str = str + "&";
         }
         // 1. If we _start_ with an implies, therefore, etc. put it in col 1.
+        // If we have a "\\displaystyle" at the start then it kills step (1) below.
+        str = str.trim();
+        var displaystyle = "";
+        if (str.startsWith("\\displaystyle")) {
+            str = str.slice("\\displaystyle".length);
+            displaystyle = "\\displaystyle";
+        }
         const imptxt = [`Rightarrow`, `Leftarrow`, `Leftrightarrow`, `therefore`, `because`];
-        const imptxttk = imptxt.flatMap(token => [`\\${token}{`, `\\${token} `]);
+        const imptxttk = imptxt.flatMap(token => [`\\${token}{`, `\\${token} `, `\\${token}\\`]);
         const matchimp = imptxttk.find(token => str.startsWith(token));
         if (matchimp === undefined) {
-            str = `& & ` + str;
+            str = "& &" + displaystyle + str;
         } else {
-            str = str.slice(0, matchimp.length) + '& &' + str.slice(matchimp.length);
+            str = str.slice(0, matchimp.length - 1) + "& &" + displaystyle + str.slice(matchimp.length - 1);
         }
         str += `\\\\`;
         output.push(str);
